bfe4dd22cc948d6e5a4c1b521168bcfeb0537a64
Bocar <bocardev@outlook.com>
2025-08-25 11:05:40 +0000

    course 1 update

--- src/main/java/com/bocardev/store/PaymentService.java
+++
@@ -1,5 +0,0 @@
-package com.bocardev.store;
-
-public interface PaymentService {
-    void processPayment(double amount);
-}
--- src/test/java/com/codewithmosh/store/StoreApplicationTests.java
+++ src/test/java/com/codewithmosh/store/StoreApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bocardev.store;
+package com.codewithmosh.store;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
